c4c052ffa5234b41886dfd987ee72e442fe95996
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-07-21 18:05:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 function threeSum(arr, target) {
 // write your code here
+
+	arr.sort((a,b)=>a-b);
+
+	let closestSum=Infinity;
+
+	for(let i=0; i<arr.length-2; i++){
+
+		let left=i+1, right=arr.length-1;
+		
+		let currentSum=arr[i]+arr[left]+arr[right];
+
+		while(left<right){
+
+			if(Math.abs(currentSum-target)<Math.abs(closestSum-target)){
+
+				closestSum=currentSum;
+			}
+			else if(currentSum>target){
+
+					right--;
+				
+			}
+			else{
+
+				left++;
+			}
+			
+			
+		}
+
+		
+	}
+
+	return closestSum;
   
 }
 
